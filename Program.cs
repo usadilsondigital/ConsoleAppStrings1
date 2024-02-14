94c0adbbb511b53556a4b02f41346f80a9ba53af
Adilson <usadilsondigital@gmail.com>
2024-02-14 13:05:13 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleAppStrings1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "The quick brown fox jumps over the lazy dog.";
+            string[] words = phrase.Split(' ');
+
+            foreach (var word in words)
+            {
+                System.Console.WriteLine($"<{word}>");
+            }
+
+
+        }
+    }
+}
