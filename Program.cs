79e316f59356eceaa7b10c4f4d447f616634600e
Adilson <usadilsondigital@gmail.com>
2024-02-15 13:22:07 -0600

    case "/"

--- Program.cs
+++ Program.cs
@@ -151,6 +151,9 @@ foreach (string expression in expressions2)
             case "*":
                 Console.WriteLine("{0} = {1}", m.Value, value1 * value2);
                 break;
+            case "/":
+                Console.WriteLine("{0} = {1:N2}", m.Value, value1 / value2);
+                break;
         }
     }
 
